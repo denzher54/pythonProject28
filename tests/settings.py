31bd2fc5447f83aa967db144b2296b9a6c1b2395
denzher54 <denzher54@yandex.ru>
2023-08-16 20:19:18 +0300

    Initial commit

---
+++ tests/settings.py
@@ -0,0 +1,26 @@
+class Settings:
+    chrome_driver = "/tests/chromedriver.exe"
+
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'denzher****@gmail.com'
+    valid_password = 'DYqExp!dQXTZUB2'
+    valid_email_reg = 'Ввести валидный email'
+    invalid_email = 'incorrect@mmail.ru'
+    invalid_password = 'inCOR4321'
+    empty_email = ''
+    empty_password = ''
+    menu_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    first_name = 'Денис'
+    last_name = 'Щербаков'
+    first_name_ = '-'
+    last_name_ = '-'
+    russian_generate_string = 'л'
+    latin_generate_string = 'incorrect'
+    chinese_chars = '形聲字象形字形声字/'
+    special_chars = '()_+=/{}!@#$%^&*'
+    numbers = 845
+    empty = ''
+    passw1 = 'QwE1jf1'
+    passw2 = 'CvhgD54ds4'
+    passw3 = 'Fgh45JHnm767SV12'
